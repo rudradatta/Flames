fc7ccf43b801dfba7bf7186b358a597fedabac08
rudradatta <44245797+rudradatta@users.noreply.github.com>
2020-04-12 04:18:35 +0530

    Add files via upload

---
+++ javascripts/application.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+	bindFormValidation();
+    });
+
+function formSubmitAction() {
+    $.post('/show',
+	   $("#flames-form").serialize(),
+	   function(data){
+	       response = JSON.parse(data);
+	       $('#result h3').html(response.flames.result);
+	       resetResultClass();
+	       $('#result').addClass(response.flames.status);
+	   }	      
+	   );
+    return false;
+}
+
+function resetResultClass(){
+    $('#result').removeClass();
+    $('#result').addClass('center');
+    $('#result').addClass('span-11');
+    $('#result').addClass('last');
+
+    setTimeout(function(){ $('#load').hide();  $('#submit').show();     $('#result').fadeIn('slow').delay(2000).fadeOut('slow'); }, 2000);
+
+}
+
+function bindFormValidation() {
+    $('#flames-form').validate({
+	    rules: {
+		your_name: "required",
+		partner_name: "required"
+		    },
+	    messages: {
+		your_name: "nam bol be",
+		partner_name: "shaqt launda hai kya"
+		    },
+		submitHandler: function(form) {
+		$('#load').show();
+		$('#submit').hide();
+		formSubmitAction();
+	    }
+	});
+}
+ 
